9665b91605b606c923d9b3839f7b9bf5d1cf6c45
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 00:44:55 +0600

    Update judge.js

--- engine/judge.js
+++ engine/judge.js
@@ -1,1 +1,25 @@
+class JudgeEngine {
 
+    constructor() {
+        this.errors = [];
+    }
+
+    addError(error) {
+        this.errors.push(error);
+    }
+
+    clear() {
+        this.errors = [];
+    }
+
+    hasErrors() {
+        return this.errors.length > 0;
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+
+}
+
+const JUDGE = new JudgeEngine();
