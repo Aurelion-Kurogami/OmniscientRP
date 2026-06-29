2681770a078578e21edc92aa5df190f282328d84
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 00:53:50 +0600

    Update character.js

--- engine/character.js
+++ engine/character.js
@@ -1,1 +1,21 @@
+class CharacterEngine {
 
+    constructor() {
+        this.loadedCharacters = [];
+    }
+
+    load(character) {
+        this.loadedCharacters.push(character);
+    }
+
+    clear() {
+        this.loadedCharacters = [];
+    }
+
+    all() {
+        return this.loadedCharacters;
+    }
+
+}
+
+const CHARACTER = new CharacterEngine();
