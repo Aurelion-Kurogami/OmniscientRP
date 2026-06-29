6948f8e5b89afd271e541a4d5ba7c5c45c44eb56
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 00:48:21 +0600

    Update router.js

--- engine/router.js
+++ engine/router.js
@@ -1,1 +1,17 @@
+class Router {
 
+    constructor() {
+        this.provider = "auto";
+    }
+
+    setProvider(provider) {
+        this.provider = provider;
+    }
+
+    getProvider() {
+        return this.provider;
+    }
+
+}
+
+const ROUTER = new Router();
