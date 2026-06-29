938f6aebea544780162d0f5d82ee978379c85bb3
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-29 20:30:41 +0600

    Update memory.js

--- engine/memory.js
+++ engine/memory.js
@@ -1,1 +1,34 @@
+class MemoryEngine {
 
+    constructor() {
+        this.memories = [];
+    }
+
+    add(memory) {
+        memory.time = Date.now();
+        this.memories.push(memory);
+    }
+
+    all() {
+        return this.memories;
+    }
+
+    search(keyword) {
+
+        return this.memories.filter(memory => {
+
+            return JSON.stringify(memory)
+                .toLowerCase()
+                .includes(keyword.toLowerCase());
+
+        });
+
+    }
+
+    clear() {
+        this.memories = [];
+    }
+
+}
+
+const MEMORY = new MemoryEngine();
