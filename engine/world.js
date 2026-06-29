66dd50a2a0aaa99133e4f0565f06ea1bd0cd0881
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 00:53:01 +0600

    Update world.js

--- engine/world.js
+++ engine/world.js
@@ -1,1 +1,22 @@
+class WorldEngine {
 
+    constructor() {
+        this.currentPack = "";
+        this.currentScenario = 1;
+    }
+
+    loadPack(pack) {
+        this.currentPack = pack;
+    }
+
+    setScenario(id) {
+        this.currentScenario = id;
+    }
+
+    getScenario() {
+        return this.currentScenario;
+    }
+
+}
+
+const WORLD = new WorldEngine();
