b56d3b749837fb8e43d4e98a0855c19c9afabedb
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 00:51:28 +0600

    Update story.js

--- engine/story.js
+++ engine/story.js
@@ -1,1 +1,21 @@
+class StoryEngine {
 
+    constructor() {
+        this.currentScene = "";
+    }
+
+    setScene(scene) {
+        this.currentScene = scene;
+    }
+
+    getScene() {
+        return this.currentScene;
+    }
+
+    clear() {
+        this.currentScene = "";
+    }
+
+}
+
+const STORY = new StoryEngine();
