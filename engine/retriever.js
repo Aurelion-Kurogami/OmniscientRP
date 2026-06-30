9cdffd3f28841a6de1e195a4f26143ba5a80f0c0
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 10:46:02 +0600

    Update retriever.js

--- engine/retriever.js
+++ engine/retriever.js
@@ -1,1 +1,30 @@
+class Retriever {
 
+    findCharacter(id) {
+
+        const characters = DB.get("characters");
+
+        return characters.find(character => character.id === id) || null;
+
+    }
+
+    retrieve(searchResult) {
+
+        if (!searchResult)
+            return null;
+
+        switch (searchResult.type) {
+
+            case "character":
+                return this.findCharacter(searchResult.id);
+
+            default:
+                return null;
+
+        }
+
+    }
+
+}
+
+const RETRIEVER = new Retriever();
