2c81825bf81c3968909d6a43ff3443c01aebb658
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 10:31:10 +0600

    Update search.js

--- engine/search.js
+++ engine/search.js
@@ -1,1 +1,19 @@
+class SearchEngine {
 
+    search(keyword) {
+
+        keyword = keyword.toLowerCase();
+
+        const index = DB.get("search_index");
+
+        return index.filter(entry =>
+            entry.keywords.some(k =>
+                k.toLowerCase().includes(keyword)
+            )
+        );
+
+    }
+
+}
+
+const SEARCH = new SearchEngine();
