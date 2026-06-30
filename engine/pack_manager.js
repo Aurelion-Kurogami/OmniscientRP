67eb20c3f115b5d65499335336189b9ee33e74e8
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 10:43:03 +0600

    Update pack_manager.js

--- engine/pack_manager.js
+++ engine/pack_manager.js
@@ -1,1 +1,21 @@
+class PackManager {
 
+    constructor() {
+        this.current = null;
+    }
+
+    async use(pack) {
+
+        await LOADER.loadPack(pack);
+
+        this.current = pack;
+
+    }
+
+    getCurrent() {
+        return this.current;
+    }
+
+}
+
+const PACK_MANAGER = new PackManager();
