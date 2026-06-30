8bf8020f5560421ada88c5a0df6c046fed46dddb
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-07-01 02:21:08 +0600

    Update config.js

--- engine/config.js
+++ engine/config.js
@@ -1,1 +1,45 @@
-world: WORLD.get(),
+export class EngineConfig {
+
+    constructor() {
+
+        this.settings = {
+
+            pack: "orv",
+
+            provider: "openrouter",
+
+            autoSave: true,
+
+            autoBehavior: true,
+
+            autoDirector: true,
+
+            autoSkillTrigger: true,
+
+            autoJudge: true,
+
+            debug: false
+
+        };
+
+    }
+
+    get(key) {
+
+        return this.settings[key];
+
+    }
+
+    set(key, value) {
+
+        this.settings[key] = value;
+
+    }
+
+    export() {
+
+        return structuredClone(this.settings);
+
+    }
+
+}
