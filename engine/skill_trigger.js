f63d588b32e56936a64a80f163d384734b0fb619
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 11:04:34 +0600

    Create skill_trigger.js

---
+++ engine/skill_trigger.js
@@ -0,0 +1,18 @@
+class SkillTriggerEngine {
+
+    check(character, world) {
+
+        const activated = [];
+
+        // Later:
+        // Read exclusive_skills.json
+        // Check trigger conditions
+        // Return activated skills
+
+        return activated;
+
+    }
+
+}
+
+const SKILLS = new SkillTriggerEngine();
