480760ea6e73ef97ff8d47a534b7ecc0e8a165d4
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-07-01 02:37:56 +0600

    Update emotion.js

--- engine/emotion.js
+++ engine/emotion.js
@@ -1,11 +1,55 @@
-class EmotionEngine {
+export class EmotionEngine {
 
-    detect(context) {
+    constructor() {
 
-        return "neutral";
+        this.emotions = new Map();
+
+    }
+
+    get(id) {
+
+        return this.emotions.get(id) || {
+
+            mood: "neutral",
+
+            intensity: 0
+
+        };
+
+    }
+
+    set(id, mood, intensity = 1) {
+
+        this.emotions.set(id, {
+
+            mood,
+
+            intensity
+
+        });
+
+    }
+
+    decay() {
+
+        for (const [id, emotion] of this.emotions) {
+
+            emotion.intensity = Math.max(
+
+                0,
+
+                emotion.intensity - 1
+
+            );
+
+            if (emotion.intensity === 0) {
+
+                emotion.mood = "neutral";
+
+            }
+
+        }
 
     }
 
 }
-
-const EMOTION = new EmotionEngine();
