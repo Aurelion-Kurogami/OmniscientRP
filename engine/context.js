d3df8d7466c7e0297d8e05ed57b14de3dabfe75b
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 21:01:55 +0600

    Update context.js

--- engine/context.js
+++ engine/context.js
@@ -1,26 +1,45 @@
+import { World } from "./world.js";
+
 export class Context {
 
     constructor() {
+
+        this.world = new World();
+
         this.state = {
+
             pack: null,
+
             timeline: null,
-            location: null,
-            scenario: null,
-            participants: [],
-            world: {}
+
+            participants: []
+
         };
+
     }
 
     set(key, value) {
+
         this.state[key] = value;
+
     }
 
     get(key) {
+
         return this.state[key];
+
     }
 
     export() {
-        return structuredClone(this.state);
+
+        return {
+
+            ...structuredClone(this.state),
+
+            world: this.world.get()
+
+        };
+
     }
 
 }
