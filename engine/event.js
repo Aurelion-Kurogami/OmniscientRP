252f40edfd76e593c7b8c093e80f08e177d3e3d2
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 20:59:43 +0600

    Update event.js

--- engine/event.js
+++ engine/event.js
@@ -1,29 +1,32 @@
-class EventEngine {
+export class EventManager {
 
     constructor() {
-
         this.events = [];
-
     }
 
-    trigger(event) {
+    add(event) {
 
-        this.events.push(event);
+        this.events.push({
+            ...event,
+            created: Date.now()
+        });
 
     }
 
     active() {
-
-        return this.events;
-
+        return [...this.events];
     }
 
     clear() {
-
         this.events = [];
+    }
+
+    remove(id) {
+
+        this.events = this.events.filter(
+            event => event.id !== id
+        );
 
     }
 
 }
-
-const EVENTS = new EventEngine();
