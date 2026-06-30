66e2e400afe4ec46818a42319174e1ec6c270413
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-06-30 10:42:21 +0600

    Update loader.js

--- engine/loader.js
+++ engine/loader.js
@@ -1,1 +1,45 @@
+class Loader {
 
+    async loadPack(pack) {
+
+        const base = `packs/${pack}`;
+
+        const files = [
+            "characters",
+            "constellations",
+            "exclusive_skills",
+            "stories",
+            "stigmas",
+            "attributes",
+            "items",
+            "organizations",
+            "places",
+            "rules",
+            "scenarios",
+            "timeline",
+            "search_index",
+            "dokkaebis",
+            "nebulae",
+            "incarnations",
+            "monsters",
+            "outer_gods",
+            "coins",
+            "probability"
+        ];
+
+        DB.clear();
+
+        for (const file of files) {
+
+            const response = await fetch(`${base}/${file}.json`);
+            const data = await response.json();
+
+            DB.register(file, data);
+
+        }
+
+    }
+
+}
+
+const LOADER = new Loader();
