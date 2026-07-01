9e2881229245546984657d8a9c31c3d851fef928
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-07-01 09:35:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-function feature(name){
-    alert(name + " is coming soon!");
-}
+window.OMNISCIENT_RP = {
+
+    version: "0.1.0-alpha",
+
+    initialized: false,
+
+    init() {
+
+        console.log("=== OmniscientRP Alpha ===");
+
+        console.log("Initializing...");
+
+        this.initialized = true;
+
+        console.log("Ready.");
+
+    },
+
+    feature(name) {
+
+        alert(name + " is coming soon!");
+
+    }
+
+};
+
+window.addEventListener("load", () => {
+
+    window.OMNISCIENT_RP.init();
+
+});
