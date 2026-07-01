351c89c9e3adeb9f9219a576923d4506e0331ce0
Aurelion-Kurogami <salmanneazrhythm@gmail.com>
2026-07-01 08:50:01 +0600

    Update canon.js

--- engine/canon.js
+++ engine/canon.js
@@ -4,10 +4,31 @@ class CanonEngine {
 
         const results = SEARCH.search(name);
 
-        if (results.length === 0)
+        if (results.length === 0) {
+
             return null;
 
+        }
+
         return RETRIEVER.retrieve(results[0]);
+
+    }
+
+    exists(name) {
+
+        return this.get(name) !== null;
+
+    }
+
+    verify(name) {
+
+        return {
+
+            exists: this.exists(name),
+
+            data: this.get(name)
+
+        };
 
     }
 
